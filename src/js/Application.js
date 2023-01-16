5009f6be2315762be99c150fea8963fd9fd7844f
Milen <milenmihalkov@gmail.com>
2023-01-16 19:56:44 +0200

    banana task

--- src/js/Application.js
+++ src/js/Application.js
@@ -14,8 +14,16 @@ export default class Application extends EventEmitter {
     this.emit(Application.events.READY);
   }
   setEmojis(emojis) {
+    let emojiDiv = document.querySelector("#emojis");
+    emojiDiv.textContent = "";
     this.emojis = emojis;
+    let newParagraph = document.createElement("p");
+    newParagraph.textContent = this.emojis.join(" ");
+    emojiDiv.appendChild(newParagraph);
   }
 
-  addBananas() {}
+  addBananas() {
+    let addbanana = this.emojis.map(m => m + this.banana);
+    this.setEmojis(addbanana);
+  }
 }
